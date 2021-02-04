8dc2761941c291c8fe583321548072ae959aca47
suwaaaa <1264584869@qq.com>
2021-02-05 03:23:40 +0800

    ZUUL ,CONFIG SERVER

---
+++ spring_cloud_api_gateway/spring_cloud_api_gateway/src/main/java/suwaaaa/author/spring_cloud_api_gateway/SpringCloudApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package suwaaaa.author.spring_cloud_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApiGatewayApplication.class, args);
+	}
+
+}
